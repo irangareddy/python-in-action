9858cf16340c369a14779f5b61ba24502ba83222
Ranga Reddy Nukala <itsmerr17@gmail.com>
2022-07-26 12:54:57 +0530

    add list_slicing and flames_game Exercise

---
+++ Exercises/flames_game.py
@@ -0,0 +1,73 @@
+import random
+
+name_1 = 'AJAY'
+name_2 = 'PRIYA'
+
+
+def convert(string):
+    list1 = []
+    list1[:0] = string
+    return list1
+
+
+list_1 = convert(name_1)
+list_2 = convert(name_2)
+
+for i in list_1:
+    for j in list_2:
+        if i == j:
+            list_1.remove(i)
+            list_2.remove(j)
+
+c = list_1 + list_2
+count = len(c)
+result = ["Friends", "Love ", " Affection ", "Marriage", "Enemy", "Siblings"]
+
+while len(result) > 1:
+    split_index = (count % len(result) - 1)
+    print(split_index)
+    if split_index >= 0:
+
+        # list slicing
+        right = result[split_index + 1:]
+        left = result[: split_index]
+        print(right,left)
+
+        # list concatenation
+        result = right + left
+    else:
+        result = result[: len(result) - 1]
+
+        # print final result
+print("Relationship status :", result[0])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Exercises/list_slicing.py
@@ -0,0 +1,11 @@
+message = "Good Morning"
+
+print(message[0:1])
+print(message[0:-1])
+print(message[11])
+# start = end index or start index
+# stop =  n-1
+# step = increment or decrement
+print(message[13:6:-3])
+
+# print(message[123]) Index Error
