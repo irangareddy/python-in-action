4148eb9adf7dabbd772ae00161cd8df86db2d0dc
Ranga Reddy Nukala <--global>
2022-08-01 22:34:51 +0530

    add assertions for data validation

--- Python Tricks/2 Patterns for Cleaner Python/2.1-assertions.py
+++ Python Tricks/2 Patterns for Cleaner Python/2.1-assertions.py
@@ -23,8 +23,8 @@ print(final_amount)
 
 
 """Python's Assert Syntax"""
-expression1 = 10 % 2 == 1
-expression2 = 10 % 2 == 0
+expression1 = 10 % 2 == 0
+expression2 = 10 % 2 == 1
 
 if __debug__ and not expression1:
     raise AssertionError(expression2)
@@ -44,3 +44,47 @@ else:
         'encounter this in the wild. Thanks!')
 
 # read more: https://dbader.org/blog/catching-bogus-python-asserts
+
+"""
+TOPIC: Common Pitfalls With Using Asserts in Python
+DATE: 2022/08/01
+
+Two Caveats
+
+1. Introducing security risks and bugs into your applications, 
+2. Syntax quirk that makes it easy to write useless assertions.
+"""
+
+# Caveat - 1 – Don’t Use Asserts for Data Validation
+
+"""
+def delete_product(prod_id, user):
+    assert user.is_admin(), 'Must be admin'
+    assert store.has_product(prod_id), 'Unknown product'
+    store.get_product(prod_id).delete()
+    
+    
+Serious Problems
+1. Checking for admin privileges with an assert statement is dangerous.
+    Reason: If assertions are disabled in the Python interpreter, this turns into a null-op. 
+    Therefore any user can now delete products. The privileges check does not even run. 
+    This likely introduces a security problem and opens the door for attackers to destroy or 
+    severely damage the data in our online store. Not good.
+2. The has_product() check is skipped when assertions are disabled.
+    Reason:This means get_product() can now be called with invalid product IDs—which could lead to more severe bugs,
+     depending on how our program is written. In the worst case, this could be an avenue for someone to launch 
+     Denial of Service attacks against our store. For example, if the store app crashes if someone attempts to delete 
+     an unknown product, an attacker could bombard it with invalid delete requests and cause an outage
+     
+     
+Key Takeaways:
+The answer is to never use assertions to do data validation. 
+Instead, we could do our validation with regular if-statements and raise validation exceptions if necessary, like so:
+
+def delete_product(product_id, user):
+    if not user.is_admin():
+        raise AuthError('Must be admin to delete')
+    if not store.has_product(product_id):
+        raise ValueError('Unknown product id')
+    store.get_product(product_id).delete()
+"""
