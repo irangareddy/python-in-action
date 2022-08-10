229a0f16c7573fe10147225a2113254501957f37
Ranga Reddy Nukala <itsmerr17@gmail.com>
2022-08-10 23:47:44 +0530

    - Dicts can be used to emulate switch/case statement - Python has a HTTP server

--- Monthly Python Tricks/aug-22.py
+++ Monthly Python Tricks/aug-22.py
@@ -21,3 +21,53 @@ def add(a, b):
 math_operations = [add]
 print(math_operations[0])
 print(math_operations[0](2, 3))
+
+"""
+TOPIC: Python has first-class functions they can be used to emulate switch/case statements
+DATE: 2022/08/05
+"""
+
+
+def dispatch_if(operator, x, y):
+    if operator == 'add':
+        return x + y
+    elif operator == 'sub':
+        return x - y
+    elif operator == 'mul':
+        return x * y
+    elif operator == 'div':
+        return x / y
+    else:
+        return None
+
+
+def dispatch_dict(operator, x, y):
+    return {
+        'add': lambda: x + y,
+        'sub': lambda: x - y,
+        'mul': lambda: x * y,
+        'div': lambda: x / y,
+    }.get(operator, lambda: None)()
+
+
+print(dispatch_if('add', 5, 1))
+print(dispatch_dict('add', 5, 1))
+print(dispatch_if('unknown', 5, 1))
+print(dispatch_dict('unknown', 5, 1))
+
+"""
+TOPIC: Python has a HTTP server
+DATE: 2022/08/09
+
+
+Python has a HTTP server built into the standard library. 
+This is super handy for previewing websites
+
+Python 3.x
+$ python3 -m http.server
+
+Python 2.x
+$ python -m SimpleHTTPServer 800
+(This will serve the current directory at http://localhost:8000)
+
+"""
