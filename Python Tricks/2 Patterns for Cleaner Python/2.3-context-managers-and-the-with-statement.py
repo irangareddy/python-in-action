df8df64d24159f4dd6dad8f682b988243429daa7
Ranga Reddy Nukala <itsmerr17@gmail.com>
2022-08-10 22:54:27 +0530

    add context example related to threading

--- Python Tricks/2 Patterns for Cleaner Python/2.3-context-managers-and-the-with-statement.py
+++ Python Tricks/2 Patterns for Cleaner Python/2.3-context-managers-and-the-with-statement.py
@@ -1,4 +1,5 @@
 """2.3 Context Managers and the with Statement"""
+import threading
 
 with open('message.txt', 'w') as f:
     f.write('Hello World!')
@@ -11,4 +12,24 @@ try:
     f.write('hello, world')
 finally:
     f.close()
+"""
+
+some_lock = threading.Lock()
+
+# Harmful:
+some_lock.acquire()
+try:
+    print('Inside Try')
+finally:
+    some_lock.release()
+
+# Better:
+with some_lock:
+    print('using with while threading')
+
+"""
+Note:
+The with statement can make code that deals with system resources more readable. 
+It also helps you avoid bugs or leaks by making it practically impossible to forget
+to clean up or release a resource when it’s no longer needed.
 """
