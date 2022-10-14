3d5238c08956c3a75391f27ae0e1cc379926b3b2
Ranga Reddy Nukala <itsmerr17@gmail.com>
2022-10-14 15:39:37 +0530

    add rectangle

--- python_in_action/excercises/multiple_inheritance.py
+++ python_in_action/excercises/multiple_inheritance.py
@@ -25,3 +25,25 @@ class Square:
 box = Square(length=4)
 print(f'The box of length {box.length} '
       f'has area: {box.area()} and perimeter: {box.perimeter()}')
+
+
+class Rectangle:
+    """ Rectangle Shape """
+
+    def __init__(self, length, width):
+        """ create a rectangle with specified length """
+        self.length = length
+        self.width = width
+
+    def area(self):
+        """returns the area"""
+        return self.length * self.width
+
+    def perimeter(self):
+        """returns the perimeter"""
+        return 4 * self.length
+
+
+big_box = Rectangle(length=4, width=8)
+print(f'The big box of length {big_box.length} and width {big_box.width} '
+      f'has area: {big_box.area()} and perimeter: {big_box.perimeter()}')
